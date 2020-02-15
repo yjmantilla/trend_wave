af7d95983d127be3550065bb638a7b7ab96639fb
ykmunoz <ykmunoz@bancolombia.com.co>
2020-02-15 16:17:44 -0500

    shape code

--- examples/shape.js
+++ examples/shape.js
@@ -8,13 +8,14 @@ function setup() {
   fill2(208, 101, 192, 127);
   stroke(127, 63, 120);
 
- 
+  // A rectangle
+  rect(40, 120, 120, 40);
 
   // A design for a simple flower
   translate(580, 200);
   noStroke();
   for (let i = 0; i < 10; i ++) {
-    rect(0, 30, 20, 80);
-    rotate(5);
+    rect(80, 30, 20, 80);
+    translate(580, 200);
   }
 }
