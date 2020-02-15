ecc2060c2168e367c1fc202417a5919cec569194
ykmunoz <ykmunoz@bancolombia.com.co>
2020-02-15 16:14:57 -0500

    shape code

--- examples/shape.js
+++ examples/shape.js
@@ -8,12 +8,7 @@ function setup() {
   fill2(208, 101, 192, 127);
   stroke(127, 63, 120);
 
-  // A rectangle
-  rect(40, 120, 120, 40);
-  // An ellipse
-  ellipse(240, 240, 80, 80);
-  // A triangle
-  triangle(300, 100, 320, 100, 310, 80);
+ 
 
   // A design for a simple flower
   translate(580, 200);
